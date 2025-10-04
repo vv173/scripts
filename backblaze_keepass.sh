026e3380cb5eaaeb77a249146727b47b9a361263
Viktor Vodnev <83870029+vv173@users.noreply.github.com>
2025-10-04 22:46:13 +0200

    Make a bash script to backup the KeePass database.

---
+++ backblaze_keepass.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Back up KeePass database to Backblaze B2 using rclone.
+
+set -euo pipefail
+
+HOME_DIR="/home/viktorvo/"
+KEEPASS_DIR="$HOME_DIR/.keepass/"
+BUCKET_NAME=""
+REMOTE_PATH="keepass:${BUCKET_NAME}/"
+
+RCLONE_OPTS=(
+    "--checksum"
+    "--human-readable"
+    "--exclude='.stfolder*'"
+    "--config=${HOME_DIR}/.config/rclone/rclone.conf"
+    "--log-file=/var/log/rclone/keepass.log"
+    "--log-file-max-size=10M"
+    "--log-level=INFO"
+    "--use-json-log"
+    "--dry-run"
+)
+
+rclone copy "${RCLONE_OPTS[@]}" "$KEEPASS_DIR" "$REMOTE_PATH"
